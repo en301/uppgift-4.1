b8c5c11dfb2a2a60cd4f1f7af0873309aaec3892
en301 <emilsjostrand1000@gmail.com>
2023-10-18 10:00:19 +0200

    Add project files.

---
+++ uppgift 4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace uppggift_4_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 10;
+            while (tal <= 30)
+            {
+                Console.WriteLine(tal);
+                tal++;
+            }
+            int tal2 = 200;
+            while (tal2 >= 180)
+            {
+                Console.WriteLine(tal2);
+                tal2--;
+            }
+            int tal3 = 1000;
+            while (tal3 <= 1400)
+            {
+                Console.WriteLine(tal3);
+                tal3+=50;
+            }
+            Console.ReadKey();
+        }
+    }
+}
